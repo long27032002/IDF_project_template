c15f214783b5686cf49995d56153fc6ee8506078
Nguyễn Như Hải Long <long27032002@gmail.com>
2023-05-01 00:55:42 +0700

    Add CMake file

---
+++ component/sensor_1/include/sensor.h
@@ -0,0 +1,14 @@
+/**
+ * @file sensor.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef __SENSOR_H__
+#define __SENSOR_H__
+
+#endif
